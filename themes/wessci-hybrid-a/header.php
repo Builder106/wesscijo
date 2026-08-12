fc15950daea038759909e75dc0736fda7353c521
Builder106 <vaughanolayinka@gmail.com>
2026-08-11 19:20:53 -0700

    Rework hybrid-a masthead into black hero + white index band

--- themes/wessci-hybrid-a/header.php
+++ themes/wessci-hybrid-a/header.php
@@ -10,21 +10,18 @@
 
 <a class="skip-link" href="#main">Skip to content</a>
 
-<header class="masthead">
-	<div class="bar bar--top">
-		<a class="wordmark" href="<?php echo esc_url( home_url( '/' ) ); ?>">
-			<span class="wordmark__mark" aria-hidden="true"></span>
-			<span class="wordmark__text">The Wesleyan Science Journal</span>
-		</a>
+<header class="hero">
+	<div class="hero__top">
+		<a class="hero__wordmark" href="<?php echo esc_url( home_url( '/' ) ); ?>">Wesleyan Science Journal</a>
 
-		<form class="search" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+		<form class="hero__search" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 			<label class="u-visually-hidden" for="s">Search the journal</label>
-			<input class="search__input" type="search" id="s" name="s" placeholder="SEARCH" value="<?php echo esc_attr( get_search_query() ); ?>">
-			<button class="search__submit" type="submit">Go</button>
+			<input class="hero__search-input" type="search" id="s" name="s" placeholder="Search" value="<?php echo esc_attr( get_search_query() ); ?>">
+			<button class="hero__search-submit" type="submit">Go</button>
 		</form>
 	</div>
 
-	<nav class="bar bar--index" aria-label="Sections">
+	<nav class="hero__index" aria-label="Sections">
 		<?php foreach ( wessci_divisions() as $div ) : ?>
 			<div class="index-group">
 				<a class="index-group__title" href="<?php echo esc_url( get_term_link( $div['term'] ) ); ?>">
